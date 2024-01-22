ac001e60c1bbbb669bcd3d5badd9529330dfea42
EmilyMontalvo25 <emily.montalvo@epn.edu.ec>
2024-01-22 03:20:23 -0500

    Add routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react'; 
 import './index.css'
+import ReactDOM from 'react-dom/client';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+
+const router = createBrowserRouter(
+[
+  {
+    path: '/',
+    element: <h1>Inicio</h1>
+  },
+  {
+    path: '/nosotros',
+    element: <h1>Nosotros</h1> 
+  }
+]
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+
   <React.StrictMode>
-    <App />
+      {/* El Router Provider va a ser el centro de la aplicacion, aqu[i comienza
+      a fluir datos sobre los demas comonentes*/}
+      {/* Espera un propt */}
+      <RouterProvider 
+      router = {router}
+      />
   </React.StrictMode>,
 )
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}' // Fijarse que no hayan espacios en blanco
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
